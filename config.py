ace8e0c97474ac17b517c3a1fadd288f4d6b2951
malymary <malyutina.mary@gmail.com>
2023-04-30 17:05:18 +0800

    main files

---
+++ config.py
@@ -0,0 +1,24 @@
+'''https://api.hh.ru/dictionaries'''
+
+# должность, профессия
+TEXT = 'Архитектор'
+# номер страницы
+PAGE = 0
+# количество элементов
+PER_PAGE = 100
+# только вакансии за последние Х дней
+PERIOD = 180
+# опыт работы
+EXPERIENCE = 'noExperience'
+# тип занятости
+EMPLOYMENT = None
+# график работы
+SCHEDULE = None
+# регион
+AREA = None
+# сфера деятельности
+INDUSTRY = '13'
+# зарплата
+SALARY = None
+# только вакансии с указанной зарплатой
+ONLY_WITH_SALARY = True
